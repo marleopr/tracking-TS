8ffc3eef3a90f0d76fd46470961aaee3b0fcb462
Márleo Piber da Rosa <marleo.piber@easy360.ind.br>
2024-08-16 17:51:24 -0300

    Adicionado variaveis de ambiente ao .ENV

--- src/constants/BASE_URL.tsx
+++ src/constants/BASE_URL.tsx
@@ -1,3 +1,3 @@
-export const BASE_URL: string = "https://api.linketrack.com/track/json?"
-export const user: string = 'marleopr@hotmail.com';
-export const token: string = '357e587e9c9cffbaa5da1ca269fe10d3630e0bb68dcd24059be9a716134d0e87';
+export const BASE_URL = process.env.REACT_APP_BASE_URL
+export const user = process.env.REACT_APP_API_USER
+export const token = process.env.REACT_APP_API_TOKEN
