aa9a2fe77e06a169e3f529905f8d04668a9faddd
Márleo Piber da Rosa <marleo.piber@easy360.ind.br>
2024-09-13 17:35:46 -0300

    Organização SVGs

--- src/pages/BuscaCep.tsx
+++ src/pages/BuscaCep.tsx
@@ -11,6 +11,11 @@ import TrackLoader from "../components/loadersButtons/TrackLoader";
 import CepHistory from "../components/cepHistory/CepHistory";
 import CepInfo from "./CepInfo";
 import { Box } from "@mui/material";
+import {
+  ArrowBackIcon,
+  NewSearchIcon,
+  SearchIcon,
+} from "../components/loadersButtons/IconsSvg";
 
 interface CepData {
   cep: string;
@@ -114,51 +119,40 @@ const BuscaCep: React.FC = () => {
       }}
     >
       <ToastContainer />
-        <CepCointainer>
+      <CepCointainer>
         <TrackButton
-        onClick={() => goToHomePage(navigate)}
-        label="Voltar"
-        style={{ minHeight: "40px" }} 
-        iconSvg={
-          <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
-        }
-      />
-          <Box alignItems="center" margin="10px">
-            {cepData ? (
-              <TrackButton
-                onClick={handleReloadBuscaCepPage}
-                label="Nova pesquisa"
-                style={{ minHeight: "40px" }} 
-                iconSvg={
-                  <path d="M463.5 224H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1c-87.5 87.5-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5z" />
+          onClick={() => goToHomePage(navigate)}
+          label="Voltar"
+          style={{ minHeight: "40px" }}
+          iconSvg={ArrowBackIcon}
+        />
+        <Box alignItems="center" margin="10px">
+          {cepData ? (
+            <TrackButton
+              onClick={handleReloadBuscaCepPage}
+              label="Nova pesquisa"
+              style={{ minHeight: "40px" }}
+              iconSvg={NewSearchIcon}
+            />
+          ) : (
+            <Box display="flex" alignItems="center" gap={2} margin="10px">
+              <TrackInput
+                placeholder="Digite o CEP aqui"
+                value={codigoCep}
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  setCodigoCep(event.target.value)
                 }
               />
-            ) : (
-              <Box
-                display="flex"
-                alignItems="center"
-                gap={2}
-                margin="10px"
-              >
-                <TrackInput
-                  placeholder="Digite o CEP aqui"
-                  value={codigoCep}
-                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                    setCodigoCep(event.target.value)
-                  }
-                />
-                <TrackButtonV2
-                  onClick={handleGetCep}
-                  disabled={!isCodigoValido}
-                  label="Buscar"
-                  iconSvg={
-                    <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM288 176c0-44.2-35.8-80-80-80s-80 35.8-80 80c0 48.8 46.5 111.6 68.6 138.6c6 7.3 16.8 7.3 22.7 0c22.1-27 68.6-89.8 68.6-138.6zm-112 0a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
-                  }
-                />
-              </Box>
-            )}
-          </Box>
-        </CepCointainer>
+              <TrackButtonV2
+                onClick={handleGetCep}
+                disabled={!isCodigoValido}
+                label="Buscar"
+                iconSvg={SearchIcon}
+              />
+            </Box>
+          )}
+        </Box>
+      </CepCointainer>
       {loading && <TrackLoader />}
 
       {!loading && cepData && (
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -4,6 +4,7 @@ import errorPageImg from "../assets/errorPageImg.webp";
 import pageNotFound from "../assets/pagina-nao-encontrada.png"
 import TrackButton from "../components/loadersButtons/TrackButton";
 import { goToHomePage } from "../routes/coordinator";
+import { ArrowBackIcon } from "../components/loadersButtons/IconsSvg";
 
 const ErrorPage: React.FC = () => {
     const navigate = useNavigate();
@@ -15,9 +16,7 @@ const ErrorPage: React.FC = () => {
              <TrackButton
         onClick={() => goToHomePage(navigate)}
         label="Voltar"
-        iconSvg={
-          <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
-        }
+        iconSvg={ArrowBackIcon}
       />
         </Main >
 
